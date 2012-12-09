5d1c3b762249c29b0ed032baeeaf7a911fb61ea9
Sajjad Anwar <sajjadkm@gmail.com>
2012-12-09 20:33:04 +0530

    Play button and year switch

--- choropleth.js
+++ choropleth.js
@@ -3,8 +3,6 @@ var data; // loaded asynchronously
 xy = d3.geo.mercator().translate([-4220,1140]).scale(21000),
 path = d3.geo.path().projection(xy);
 
-// var path = d3.geo.path();
-
 var svg = d3.select("#chart")
   .append("svg");
 
@@ -46,13 +44,37 @@ function change_year(a) {
   d3.json("data/data"+a+".json", function(json) {
     data = json;
     districts.selectAll("path")
-    .transition()
-    .style("opacity", 0.1)
-    .transition()
-    .delay(300)
+    // .transition()
+    // .style("opacity", 0.1)
+    // .transition()
+    // .delay(300)
     .attr("class", quantize)
-    .transition()
-    .duration(1000)
-    .style("opacity", 1);
+    // .transition()
+    // .duration(1000)
+    // .style("opacity", 1);
     })
   }
+
+var start = d3.select("#start");
+start.on("click", play);
+years = ['04-05', '05-06', '06-07', '07-08', '08-09', '09-10', '10-11'];
+var i = 0;
+
+function play() {
+  d3.select(".play").classed("btn-info", false);
+  change_year(years[i]);
+  setTimeout(update_year, 2000);
+
+ };
+
+function update_year() {
+  i = i+1;
+  if (i!=years.length) {
+    play();
+  }
+  else {
+    i=0;
+    d3.select(".play").classed("btn-info", true);
+  }
+}
+
