6c39c93270b84f2e9f1c6714ede25749df7f7612
Sajjad Anwar <sajjadkm@gmail.com>
2012-12-10 17:06:07 +0530

    Click event, update selected district info across years, animated

--- choropleth.js
+++ choropleth.js
@@ -1,4 +1,7 @@
 var data; // loaded asynchronously
+var selected_district = null;
+var clicked_flag = false;
+var selected_district_data = null;
 
 xy = d3.geo.mercator().translate([-4220,1140]).scale(21000),
 path = d3.geo.path().projection(xy);
@@ -10,15 +13,20 @@ var districts = svg.append("g")
     .attr("id", "districts")
     .attr("class", "Blues");
 
+all_districts = districts.selectAll("path");
+
 d3.json("data/districts.json", function(json) {
-  districts.selectAll("path")
+  all_districts
     .data(json.features)
     .enter().append("path")
     .attr("class", data ? quantize : null)
     .attr("d", path)
     .on("mouseover", mouseover)
-    .on("mouseout", mouseout);
+    .on("mouseout", mouseout)
+    .on("click", function(d,i) { clicked(d, i, this); });
+
 });
+
 
 d3.json("data/data04-05.json", function(json) {
   data = json;
@@ -46,6 +54,28 @@ function mouseout(){
   d3.select("#info").classed("hide", true);
   d3.select("#intro").classed("hide", false);
 }
+
+function clicked(d, i, district){
+  mouseover(d);
+  selected_district_data = d;
+  if (clicked_flag == false) {
+    selected_district = district;
+    d3.select(district).classed("clicked", true);
+    districts.selectAll("path").on("mouseout", null);
+    districts.selectAll("path").on("mouseover", null);
+    clicked_flag = true;
+  }
+  else {
+  old_district = selected_district;
+  selected_district = district;
+  d3.select(old_district).classed("clicked", false);
+  d3.select(district).classed("clicked", true);
+  all_districts
+  .on("mouseover", mouseover)
+  .on("mouseout", mouseout);
+  };
+}
+
 function change_year(a) {
   d3.selectAll(".label").classed("label-info", false);
   d3.select(".year"+a).classed("label-info", true);
@@ -61,6 +91,10 @@ function change_year(a) {
     // .duration(1000)
     // .style("opacity", 1);
     })
+    if (selected_district_data) {
+    d3.select(selected_district).classed("clicked", true);
+    mouseover(selected_district_data);
+    };
   }
 
 var start = d3.select("#start");
